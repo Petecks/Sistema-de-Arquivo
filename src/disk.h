055ebc8a6bc6da091e555ae9b0947501d86b6b64
srengers <vtr_dg95@hotmail.com>
2018-06-26 11:39:13 -0300

    mudei identação

--- src/disk.h
+++ src/disk.h
@@ -3,11 +3,11 @@
 
 const unsigned int DISK_BLOCK_SIZE = 4096;
 
-int  disk_init( const char *filename, int nblocks );
-int  disk_size();
-void disk_read( int blocknum, char *data );
-void disk_write( int blocknum, const char *data );
-void disk_close();
+int  disk_init (const char *filename, int nblocks);
+int  disk_size ();
+void disk_read (int blocknum, char *data);
+void disk_write (int blocknum, const char *data);
+void disk_close ();
 
 
 #endif
